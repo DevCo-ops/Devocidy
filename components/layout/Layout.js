075ee544f7a30ea01905bff4cde75032ed088c1e
Cameron Leverett <camrbo@gmail.com>
2021-03-27 22:45:49 -0700

    Organized code to look more concise

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,28 +1,18 @@
 import Container from '@material-ui/core/Container';
-import {Fragment} from 'react'
 import NavBar from './NavBar';
 import Footer from './Footer';
-import Contributors from './Contributors';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-	main : {
-		backgroundColor : theme.palette.primary,
-    flex: 1,
-
-	},
-}));
+import Contributors from './Contributors'
+import styles from '../../styles/app.module.css'
 
 const Layout = (props) => {
 	const { children } = props;
-	const classes = useStyles();
 	return (
-		<div>
+		<div className={styles.container}>
 			<NavBar />
-			<Container maxWidth='lg' className={classes.main}>
+			<Container maxWidth='lg' className={styles.main}>
 				{children}
 			</Container>
-			<Footer discription={'This is our comunity website'}>
+			<Footer className={styles.footer}>
 				<Contributors />
 			</Footer>
 		</div>
