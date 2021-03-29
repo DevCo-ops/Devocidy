c714adbbf96a3a40da5ad8c113c5bbf82345a088
Cameron Leverett <camrbo@gmail.com>
2021-03-28 19:54:27 -0700

    added differnt styling to attempt bug fix

--- components/layout/NavBar.js
+++ components/layout/NavBar.js
@@ -1,25 +1,41 @@
+import {Fragment} from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs'
-import NavTab from '../UI/NavTab'
-import {makeStyles} from '@material-ui/core/styles'
+import Tabs from '@material-ui/core/Tabs';
+import NavTab from '../UI/NavTab';
+import LogoLink from '../UI/LogoLink';
 
 const useStyles = makeStyles((theme) => ({
-  main: {
-    backgroundColor: theme.palette.primary.light
+	main: {
+		backgroundColor: theme.palette.primary.light,
+		display: 'flex',
+		flexDirection: 'column',
+		position: 'fixed',
+	},
+	tabs: {
+		flex: 1,
+		bottom: 0,
+		paddingLeft: theme.spacing(0),
+	},
+  div:{
+    margin: theme.spacing(4)
   }
-}))
+}));
 
 const NavBar = () => {
-  const classes = useStyles()
-  return (
-   <AppBar position="static" color="primary" className={classes.main}>
-     <Tabs>
-       <NavTab name={"Home"} location={"/"} />
-       <NavTab name={"Groups"} location={"/groups"} />
-       <NavTab name={"Login"} location={"/login"} />
-     </Tabs>
-   </AppBar>
-  )
-}
+	const classes = useStyles();
+	return (
+		<Fragment>
+			<AppBar position='static' color='primary' className={classes.main}>
+				<Tabs className={classes.tabs}>
+					<NavTab name={''} pathname='' bool={true} />
+					<LogoLink />
+					<NavTab name={'Groups'} pathname={'/groups'} bool={false} />
+				</Tabs>
+			</AppBar>
+      <div className={classes.div} />
+		</Fragment>
+	);
+};
 
-export default NavBar
+export default NavBar;
