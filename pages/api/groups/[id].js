9dbfd7d00a6d86d295fc23b55988675ae41f2a53
RTEYL <tyler-w-jones@hotmail.com>
2021-03-29 23:09:21 -0700

    add crud actions to groups

---
+++ pages/api/groups/[id].js
@@ -0,0 +1,79 @@
+import dbConnect from "../../../utils/dbConnect";
+import mongoose from "mongoose";
+import Group from "../../../models/Group";
+
+export default async function handler(req, res) {
+  const {
+    query: { id },
+    method,
+    body,
+  } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case "GET":
+      await Group.aggregate(
+        [
+          { $match: { _id: mongoose.Types.ObjectId(id) } },
+          {
+            $lookup: {
+              from: "users",
+              localField: "users",
+              foreignField: "_id",
+              as: "users",
+            },
+          },
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+            },
+          },
+          { $unwind: "$owner" },
+        ],
+        (err, group) => {
+          if (err)
+            res.status(500).json({
+              err,
+              message: "server could not find group by id",
+            });
+          res.status(200).json(group);
+        }
+      );
+      break;
+
+    case "PUT":
+      await Group.findByIdAndUpdate(
+        id,
+        body,
+        { new: true, runValidators: true },
+        (err, group) => {
+          if (err)
+            res.status(500).json({
+              err,
+              message: "server could not find group by id",
+            });
+          res.status(200).json(group);
+        }
+      );
+      break;
+
+    case "DELETE":
+      const group = await Group.deleteOne({ _id: id });
+      if (!group)
+        res
+          .status(500)
+          .json({ message: "server was unable to find or delete the group" });
+      res.status(200).json({ success: true, group });
+      break;
+
+    default:
+      res.status(400).json({
+        message: `${method} Is either not allowed or not a valid request method.`,
+      });
+      break;
+  }
+}
