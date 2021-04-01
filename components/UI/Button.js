753fb40fed5bb4b24763a2bfb6b417f2cb461f58
Cameron Leverett <camrbo@gmail.com>
2021-03-31 21:21:33 -0700

    added button for any use case uses MUI button

---
+++ components/UI/Button.js
@@ -0,0 +1,28 @@
+import {default as MuiButton} from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from 'next/link'
+
+const useStyles = makeStyles((theme) => ({
+	btn: {
+    textDecoration: 'none',
+    fontSize: '30px',
+  },
+}));
+
+const Button = (props) => {
+  const classes = useStyles()
+	if (props.link) {
+		return (
+				<Link href={props.link}>
+					<a className={classes.btn}>{props.children}</a>
+				</Link>
+		);
+	}
+
+	return (
+		<MuiButton className={classes.btn}>
+			{props.children}
+		</MuiButton>
+	);
+};
+export default Button;
