0b5a1916590343d6182ed87d96331eb282e53edc
Cameron Leverett <camrbo@gmail.com>
2021-04-03 13:47:20 -0700

    ran prettier.

--- components/Goups/group-head.js
+++ components/Goups/group-head.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
-import Button from '../UI/Button'
+import Button from '../UI/Button';
 import CardHeader from '@material-ui/core/CardHeader';
 import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const GroupHead = (props) => {
-	const explore = `groups/${props._id}`
-	return (
-		<React.Fragment>
-			<CardHeader
-				action={
-					<IconButton aria-label='settings'>
-						<MoreVertIcon />
-					</IconButton>
-				}
-				title={props.name}
-			/>
-			<Button link={explore}>
-				{"See more..."}
-			</Button>
-		</React.Fragment>
-	);
+  const explore = `groups/${props._id}`;
+  return (
+    <React.Fragment>
+      <CardHeader
+        action={
+          <IconButton aria-label='settings'>
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={props.name}
+      />
+      <Button link={explore}>{'See more...'}</Button>
+    </React.Fragment>
+  );
 };
 
 export default GroupHead;
--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
-import theme from '../components/MUI/theme'
+import theme from '../components/MUI/theme';
 
 export default class MyDocument extends Document {
-  render() {
+  render () {
     return (
-      <Html lang="en">
+      <Html lang='en'>
         <Head>
           {/* PWA primary color */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" type="image/x-icon" href="images/favicon.jpg" />
+          <meta name='theme-color' content={theme.palette.primary.main} />
+          <link rel='shortcut icon' type='image/x-icon' href='images/favicon.jpg' />
           <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
           />
         </Head>
         <body>
@@ -64,6 +64,9 @@ MyDocument.getInitialProps = async (ctx) => {
   return {
     ...initialProps,
     // Styles fragment is rendered after the app and page rendering finish.
-    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
   };
 };
