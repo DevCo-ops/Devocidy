b41274b85ce11bdccd40e05792e6f971e77c80ed
RTEYL <tyler-w-jones@hotmail.com>
2021-04-03 15:22:43 -0700

    create actions for new groups

--- pages/api/groups/index.js
+++ pages/api/groups/index.js
@@ -1,8 +1,9 @@
 import dbConnect from "../../../utils/dbConnect";
+import mongoose from "mongoose";
 import Group from "../../../models/Group";
 
 export default async (req, res) => {
-  const { method } = req;
+  const { method, body } = req;
   await dbConnect();
   switch (method) {
     case "GET":
@@ -36,6 +37,44 @@ export default async (req, res) => {
         }
       );
     case "POST":
+      let newGroup = await new Group({
+        name: body.name,
+        owner: [body.ownerId],
+        image: body.image,
+        projectDescription: body.projectDescription,
+      });
+      newGroup.users.push(body.ownerId);
+      await newGroup.save();
+      await Group.aggregate(
+        [
+          { $match: { _id: mongoose.Types.ObjectId(newGroup._id) } },
+          {
+            $lookup: {
+              from: "users",
+              localField: "users",
+              foreignField: "_id",
+              as: "users",
+            },
+          },
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+            },
+          },
+          { $unwind: "$owner" },
+        ],
+        (err, group) => {
+          if (err)
+            res.status(500).json({
+              err,
+              message: "server could not find group by id",
+            });
+          res.status(200).json(group);
+        }
+      );
       break;
     default:
       res.status(400).json({
