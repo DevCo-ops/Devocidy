7f909b6899c0a8171208a86f514fc02bd620155a
RTEYL <rteyl1690@gmail.com>
2021-04-08 17:21:42 -0700

    update actions and reducers

--- store/groups/actions.js
+++ store/groups/actions.js
@@ -1,27 +1,40 @@
 export const groupActionsTypes = {
-  ALL: "ALL",
-  UPDATE_BY_ID: "UPDATE_BY_ID",
-  UPDATE_BY_EMAIL: "UPDATE_BY_EMAIL",
-  FIND_BY_ID: "FIND_BY_ID",
-  FIND_BY_EMAIL: "FIND_BY_EMAIL",
+  ALL: 'ALL',
+  UPDATE: 'UPDATE',
+  FIND_BY_ID: 'FIND_BY_ID',
 };
 
-export const getGroups = () => (dispatch) => {
-  return dispatch({ type: groupActionsTypes.ALL });
+export const getGroups = () => {
+  return async (dispatch) => {
+    await fetch('http://localhost:3000/api/groups', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: groupActionsTypes.ALL, data });
+      })
+      .catch((err) => console.log('-----------------ERROR:  ', err));
+  };
 };
 
-export const updateGroupById = (id) => (dispatch) => {
-  return dispatch({ type: groupActionsTypes.UPDATE_BY_ID, id });
-};
-
-export const updateGroupByEmail = (email) => (dispatch) => {
-  return dispatch({ type: groupActionsTypes.UPDATE_BY_EMAIL, email });
+export const updateGroup = (data) => (dispatch) => {
+  fetch(`http://localhost:3000/api/groups/${action.data._id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(action.data),
+  })
+    .then((res) => res.json())
+    .then((group) => {
+      dispatch({ type: groupActionsTypes.UPDATE, data });
+    });
 };
 
 export const findGroupById = (id) => (dispatch) => {
   return dispatch({ type: groupActionsTypes.FIND_BY_ID, id });
-};
-
-export const findGroupByEmail = (email) => (dispatch) => {
-  return dispatch({ type: groupActionsTypes.FIND_BY_EMAIL, email });
 };
--- store/users/actions.js
+++ store/users/actions.js
@@ -1,12 +1,21 @@
 export const userActionsTypes = {
-  ALL: "ALL",
-  UPDATE_BY_ID: "UPDATE_BY_ID",
-  UPDATE_BY_EMAIL: "UPDATE_BY_EMAIL",
-  FIND_BY_ID: "FIND_BY_ID",
-  FIND_BY_EMAIL: "FIND_BY_EMAIL",
+  ALL: 'ALL',
+  UPDATE_BY_ID: 'UPDATE_BY_ID',
+  UPDATE_BY_EMAIL: 'UPDATE_BY_EMAIL',
+  FIND_BY_ID: 'FIND_BY_ID',
+  FIND_BY_EMAIL: 'FIND_BY_EMAIL',
 };
 
 export const getUsers = () => (dispatch) => {
+  fetch('http://localhost:3000/pages/api/users', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  }).then((userData) => {
+    return { ...state, users: userData };
+  });
   return dispatch({ type: userActionsTypes.ALL });
 };
 
