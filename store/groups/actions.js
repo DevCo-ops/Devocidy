25899f4a61d1293419f23b22bdbaba091bb2d954
RTEYL <rteyl1690@gmail.com>
2021-04-09 18:30:00 -0700

    update CRUD actions

--- store/groups/actions.js
+++ store/groups/actions.js
@@ -1,27 +1,88 @@
 export const groupActionsTypes = {
-  ALL: "ALL",
-  UPDATE_BY_ID: "UPDATE_BY_ID",
-  UPDATE_BY_EMAIL: "UPDATE_BY_EMAIL",
-  FIND_BY_ID: "FIND_BY_ID",
-  FIND_BY_EMAIL: "FIND_BY_EMAIL",
+  ALL: 'ALL',
+  CREATE: 'CREATE',
+  UPDATE: 'UPDATE',
+  DELETE_BY_ID: 'DELETE_BY_ID',
 };
 
-export const getGroups = () => (dispatch) => {
-  return dispatch({ type: groupActionsTypes.ALL });
+export const getGroups = () => {
+  return async (dispatch) => {
+    await fetch('http://localhost:3000/api/groups', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: groupActionsTypes.ALL, data });
+      })
+      .catch((err) => console.log('-----------------ERROR:  ', err));
+  };
 };
 
-export const updateGroupById = (id) => (dispatch) => {
-  return dispatch({ type: groupActionsTypes.UPDATE_BY_ID, id });
+export const createGroup = (data) => {
+  return async (dispatch) => {
+    fetch('http://localhost:3000/api/groups', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: groupActionsTypes.CREATE, data });
+      });
+  };
 };
 
-export const updateGroupByEmail = (email) => (dispatch) => {
-  return dispatch({ type: groupActionsTypes.UPDATE_BY_EMAIL, email });
+export const updateGroup = (data) => {
+  return async (dispatch) => {
+    fetch(`http://localhost:3000/api/groups/${data._id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: groupActionsTypes.UPDATE, data });
+      });
+  };
 };
 
-export const findGroupById = (id) => (dispatch) => {
-  return dispatch({ type: groupActionsTypes.FIND_BY_ID, id });
+export const findGroupById = (id) => {
+  return async (dispatch) => {
+    await fetch(`http://localhost:3000/api/groups/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: groupActionsTypes.FIND_BY_ID, data });
+      });
+  };
 };
 
-export const findGroupByEmail = (email) => (dispatch) => {
-  return dispatch({ type: groupActionsTypes.FIND_BY_EMAIL, email });
+export const deleteGroupById = (id) => {
+  return (dispatch) => {
+    fetch(`http://localhost:3000/api/groups/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((id) => {
+        dispatch({ type: groupActionsTypes.DELETE_BY_ID, id });
+      });
+  };
 };
