da5a9fed22a2b0775fcfdb8bc00a9b48590f5fb6
RTEYL <rteyl1690@gmail.com>
2021-04-09 18:36:46 -0700

    update reducer case functions

--- store/groups/reducer.js
+++ store/groups/reducer.js
@@ -1,20 +1,41 @@
-import { groupActionsTypes } from "./actions";
+import { groupActionsTypes } from './actions';
 
 const initialState = {
-  groups: {},
+  groups: [],
 };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case groupActionsTypes.ALL:
-      fetch("../../pages/api/groups").then((groupData) => {
-        return { ...state, groups: groupData };
-      });
+      return { ...state, groups: action.data };
+      break;
 
-    case groupActionsTypes.UPDATE_BY_ID:
-    case groupActionsTypes.UPDATE_BY_EMAIL:
-    case groupActionsTypes.FIND_BY_ID:
-    case groupActionsTypes.FIND_BY_EMAIL:
+    case groupActionsTypes.CREATE:
+      return { ...state, groups: state.groups.concat(action.data) };
+      break;
+
+    case groupActionsTypes.UPDATE:
+      let updateIndex = state.groups.findIndex(
+        (group) => group._id === action.data._id
+      );
+      return {
+        ...state,
+        groups: [
+          ...state.groups.slice(0, updateIndex),
+          action.data,
+          ...state.groups.slice(updateIndex + 1),
+        ],
+      };
+      break;
+
+    // case groupActionsTypes.FIND_BY_ID:
+
+    case groupActionsTypes.DELETE_BY_ID:
+      return {
+        ...state,
+        groups: [...state.groups.filter((group) => group._id !== action.id)],
+      };
+      break;
 
     default:
       return state;
