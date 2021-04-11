3868cb48c7f7f3748f0d92d6972033f07e644e2d
RTEYL <rteyl1690@gmail.com>
2021-04-10 17:21:16 -0700

    fix various bugs with next-auth custom adapter

--- models/User.js
+++ models/User.js
@@ -1,19 +1,30 @@
-import Adapters from 'next-auth/adapters';
+import mongoose from 'mongoose';
 
-// Extend the built-in models using class inheritance
-export default class User extends Adapters.TypeORM.Models.User.model {
-  // You can extend the options in a model but you should not remove the base
-  // properties or change the order of the built-in options on the constructor
-  constructor (name, email, image, emailVerified) {
-    super(name, email, image, emailVerified);
-  }
-}
-
-export const UserSchema = {
-  name: 'User',
-  target: User,
-  columns: {
-    ...Adapters.TypeORM.Models.User.schema.columns,
-    // Adds a properties to the User schema
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    groups: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'groups',
+      },
+    ],
+    ownedGroups: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'groups',
+      },
+    ],
   },
-};
+  { timestamps: true }
+);
+
+export default mongoose.models.User || mongoose.model('User', UserSchema);
