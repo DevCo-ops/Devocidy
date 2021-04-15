1614a339877180d13879b89fbbdf5ca284083bbe
RTEYL <rteyl1690@gmail.com>
2021-04-14 21:07:03 -0700

    update action types ref: a7b0d38173b3d734e6595c973f4445c1960a02c2

--- store/groups/actions.js
+++ store/groups/actions.js
@@ -1,8 +1,10 @@
+import { updateUser } from '../users/actions';
+
 export const groupActionsTypes = {
-  ALL: 'ALL',
-  CREATE: 'CREATE',
-  UPDATE: 'UPDATE',
-  DELETE_BY_ID: 'DELETE_BY_ID',
+  ALL_GROUPS: 'ALL_GROUPS',
+  CREATE_GROUP: 'CREATE_GROUP',
+  UPDATE_GROUP: 'UPDATE_GROUP',
+  DELETE_GROUP_BY_ID: 'DELETE_GROUP_BY_ID',
 };
 
 export const getGroups = () => {
@@ -16,13 +18,17 @@ export const getGroups = () => {
     })
       .then((res) => res.json())
       .then((groups) => {
-        dispatch({ type: groupActionsTypes.ALL, groups });
+        dispatch({ type: groupActionsTypes.ALL_GROUPS, groups });
       })
       .catch((err) => console.log('-----------------ERROR:  ', err));
   };
 };
 
-export const createGroup = (group) => {
+export const createGroup = (group, user) => {
+  group.owner = user._id;
+  group.users.push(user._id);
+  user.ownedGroups.push(group._id);
+  user.groups.push(group._id);
   return (dispatch) => {
     fetch('http://localhost:3000/api/groups', {
       method: 'POST',
@@ -34,43 +40,50 @@ export const createGroup = (group) => {
     })
       .then((res) => res.json())
       .then((group) => {
-        dispatch({ type: groupActionsTypes.CREATE, group });
-      });
+        dispatch({ type: groupActionsTypes.CREATE_GROUP, group });
+        dispatch(updateUser(user));
+      })
+      .catch((err) => console.log('-----------------ERROR:  ', err));
   };
 };
 
-export const updateGroup = (group) => (dispatch) => {
-  fetch(`http://localhost:3000/api/groups/${group._id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(group),
-  })
-    .then((res) => res.json())
-    .then((group) => {
-      dispatch({ type: groupActionsTypes.UPDATE, group });
-    });
+export const updateGroup = (group) => {
+  return (dispatch) => {
+    fetch(`http://localhost:3000/api/groups/${group._id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(group),
+    })
+      .then((res) => res.json())
+      .then((group) => {
+        dispatch({ type: groupActionsTypes.UPDATE_GROUP, group });
+      })
+      .catch((err) => console.log('-----------------ERROR:  ', err));
+  };
 };
 
-export const findGroupById = (id) => (dispatch, getState) => {
-  const groups = getState().groups.groups;
-  const group = groups.filter((group) => group._id === id);
+export const findGroupById = (id) => {
+  return (dispatch, getState) => {
+    let groups = getState().groups.groups;
+    let group = groups.filter((group) => group._id === id);
 
-  if (group) return group;
+    if (group) return group;
 
-  fetch(`http://localhost:3000/api/groups/${id}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-  })
-    .then((res) => res.json())
-    .then((group) => {
-      dispatch({ type: groupActionsTypes.CREATE, group });
+    fetch(`http://localhost:3000/api/groups/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
     })
-    .catch((err) => console.log('-----------------ERROR:  ', err));
+      .then((res) => res.json())
+      .then((group) => {
+        dispatch({ type: groupActionsTypes.CREATE_GROUP, group });
+      })
+      .catch((err) => console.log('-----------------ERROR:  ', err));
+  };
 };
 
 export const deleteGroupById = (id) => {
@@ -83,7 +96,8 @@ export const deleteGroupById = (id) => {
     })
       .then((res) => res.json())
       .then((id) => {
-        dispatch({ type: groupActionsTypes.DELETE_BY_ID, id });
-      });
+        dispatch({ type: groupActionsTypes.DELETE_GROUP_BY_ID, id });
+      })
+      .catch((err) => console.log('-----------------ERROR:  ', err));
   };
 };
