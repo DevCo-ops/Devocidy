338c221bae3be5c1162a7c72c188c9f18010f706
RTEYL <rteyl1690@gmail.com>
2021-04-22 20:03:30 -0700

    fix to return connection instance to close tests

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 async function dbConnect() {
   if (mongoose.connection.readyState >= 1) {
-    return mongoose.connection.db;
+    return mongoose.connection;
   } else {
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
@@ -11,7 +11,7 @@ async function dbConnect() {
       useCreateIndex: true,
       poolSize: 10,
     });
-    return mongoose.connection.db;
+    return mongoose.connection;
   }
 }
 
